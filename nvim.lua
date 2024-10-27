486f63e8df1e602eaac381dd73a17873b2c76835
Carlos Florêncio <carlosmflorencio@gmail.com>
2024-10-27 16:10:28 +0000

    panes

--- nvim.lua
+++ nvim.lua
@@ -8,6 +8,23 @@ local function is_vim(pane)
 	return pane:get_user_vars().IS_NVIM == "true"
 end
 
+-- TODO: check if there are more than 1 pane in the tab, if no, fish accept suggestion
+local function panes_count(window)
+	-- Get the current tab
+	local tab = window.active_tab()
+
+	-- Get the panes in the tab
+	local panes = tab:panes()
+
+	-- Count the number of panes
+	local paneCount = 0
+	for _, pane in ipairs(panes) do
+		paneCount = paneCount + 1
+	end
+
+	return paneCount
+end
+
 local direction_keys = {
 	h = "Left",
 	j = "Down",
