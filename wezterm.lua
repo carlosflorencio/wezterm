b6471637eaa78469f586cfd8aa4f63709199a5c7
Carlos Florêncio <carlosmflorencio@gmail.com>
2026-05-17 10:05:50 +0100

    Updated files 2026-05-17 10:05:50

--- wezterm.lua
+++ wezterm.lua
@@ -43,9 +43,9 @@ config.scrollback_lines = 50000
 
 -- Custom quick select pattern for git branches
 config.quick_select_patterns = {
-  -- Match feature/ or fix/ branch names up to whitespace
-  -- not needed, matched by default patterns
-  -- "(?:feature|fix)/\\S+",
+	-- Match feature/ or fix/ branch names up to whitespace
+	-- not needed, matched by default patterns
+	-- "(?:feature|fix)/\\S+",
 }
 
 return config
